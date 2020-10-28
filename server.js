428413d058e4a80d900ff193d2d80aeff98d16eb
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-10-28 09:56:13 -0700

    .

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  socket.emit("id", socket.id);
+  socket.on("send message", (body) => {
+    io.emit("message", body);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server is running on PORT 5000");
+});
